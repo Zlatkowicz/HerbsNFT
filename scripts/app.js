585b9af22a023233198b9622457fb1fae6baf145
Armando <armandozlatkowicz@gmail.com>
2024-02-26 20:05:15 -0300

    ChainLink installed

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,6 @@ let account;
 /* Resuelto con un task en hardhat config */
 /* let balance = web3.eth.getBalance(0x5FbDB2315678afecb367f032d93F642f64180aa3); */
 
-let metamask;
 const connectMetamask = async () => {
     if(window.ethereum !== undefined) {
         const accounts = await ethereum.request({method: "eth_requestAccounts"});
@@ -17,6 +16,7 @@ const connectMetamask = async () => {
 let web3;
 let abi_nft = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "form", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "HerbsNFTS", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "_herbs", "type": "string" } ], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "_tokenId", "type": "uint256" } ], "name": "ownerOf", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "setName", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "setSymbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "_index", "type": "uint256" } ], "name": "tokenByIndex", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_owner", "type": "address" }, { "internalType": "uint256", "name": "_index", "type": "uint256" } ], "name": "tokenOfOwnerByIndex", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ] ;
 let nft_addr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 const mint = async () => {
 	web3 = new Web3(window.ethereum);// Para usar el provider que inyecta metamask y la libreri­a de web3
 	nft = new web3.eth.Contract(abi_nft,nft_addr);
